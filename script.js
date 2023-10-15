4349e024cda329261160eb811683271ce8da7956
Mateusz <108770502+musior@users.noreply.github.com>
2023-10-15 11:05:09 +0200

    Random border color

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('Hello from Nerdbord!')
+
+setInterval(() => {
+  const box = document.querySelector('.box');
+  const colors = ['red', 'blue', 'yellow', 'purple'];
+
+  let randomIndex = Math.floor(Math.random() * colors.length);
+
+  box.style.borderColor = colors[randomIndex];
+}, 3000);
